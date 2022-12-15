3e8c37c7560784da2017fa1629873e5c3f6e7149
Eliezer Ferreira <eliezerferreira@MacBook-Pro-de-Eliezer.local>
2022-12-15 16:25:01 -0300

    First commit

---
+++ Netflix Clone/Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Eliezer Ferreira on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
